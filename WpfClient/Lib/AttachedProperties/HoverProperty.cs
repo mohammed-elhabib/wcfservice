470c12fb0b0fe17134de094abd57666723e27a12
mohammedkah@outlook.com <mohammedkah@outlook.com>
2019-11-13 15:27:06 +0100

    style table

---
+++ WpfClient/Lib/AttachedProperties/HoverProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfClient.Lib.AttachedProperties
+{  
+    public class HoverProperty : BaseAttachedProperty<HoverProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var button = (Button)sender;
+            if ((bool)e.NewValue)
+            {
+                var colorbacground = button.Background;
+                MessageBox.Show("on changed");
+                button.Background = button.Foreground;
+                button.Foreground = colorbacground;
+            }
+        }
+/*        public override void OnValueUpdated(DependencyObject sender, object value)
+        {
+            var button = (Button)sender;
+            if ((bool)value)
+            {
+                MessageBox.Show("on update");
+                var colorbacground = button.Background;
+                button.Background = button.Foreground;
+                button.Foreground = colorbacground;
+            }
+        }*/
+    }
+}
---
+++ WpfClient/Lib/Helpers/ButtonsHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfClient.Lib.Helpers
+{
+    public class ButtonsHelper : DependencyObject
+    {
+
+
+        public static Brush GetHoverBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HoverBrushProperty);
+        }
+
+        public static void SetHoverBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HoverBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for HoverBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HoverBrushProperty =
+            DependencyProperty.RegisterAttached("HoverBrush", typeof(Brush), typeof(ButtonsHelper), new PropertyMetadata(Brushes.White));
+
+
+    }
+
+}
